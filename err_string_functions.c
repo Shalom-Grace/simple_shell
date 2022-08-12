700149c41932cd44d728d0cf4d7c3cfbbf505470
MaymaCammillus <105177160+MaymaCammillus@users.noreply.github.com>
2022-08-12 14:48:28 +0300

    Update err_string_functions.c

--- err_string_functions.c
+++ err_string_functions.c
@@ -3,7 +3,6 @@
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
- *
  * Return: Nothing
  */
 void _eputs(char *str)
@@ -23,8 +22,7 @@ void _eputs(char *str)
  * _eputchar - writes the character c to stderr
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1 and -1 otherwise
  */
 int _eputchar(char c)
 {
